c900ac15f20bb263aeca0fc7ba517dcfe45e4aad
undarmin <tejasondg@Gmail.com>
2024-03-08 12:33:58 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: "./src/index.html",
+      },
+    ),
+  ],
+};
